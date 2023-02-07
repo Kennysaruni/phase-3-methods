2fc567a3446cc4befb800cdd65623f599ee55289
Kennysaruni <kenny.saruni@student.moringaschool.com>
2023-02-08 00:32:31 +0300

    Done with lab

--- methods.rb
+++ methods.rb
@@ -1,1 +1,17 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+def greet(name)
+    puts " Hello, #{name}!"
+end
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+    puts "Hello, programmer!"
+end
+def add (a,b)
+    return a+b
+end
+def halve (num)
+    num.class == Integer ? num/2 : nil
+end
